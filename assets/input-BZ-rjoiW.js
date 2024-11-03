db76ea8f88d6678f5ec9a1f81ff39f3ceba410db
vladi <vladimir.seleznev.02@mail.ru>
2024-11-04 00:25:19 +0400

    Updates

---
+++ assets/input-BZ-rjoiW.js
@@ -0,0 +1,12 @@
+import{ao as s,ap as a}from"./index-DYmSg1OS.js";const d=s.input`
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    outline: none;
+    &:focus {
+        border-color: #40a9ff;
+        box-shadow: 0 0 5px rgba(64, 169, 255, 0.5);
+    }
+`,n=({value:o,placeholder:t,onChange:r,type:p="text"})=>a.jsx(d,{value:o,onChange:r,placeholder:t,type:p});export{n as default};
